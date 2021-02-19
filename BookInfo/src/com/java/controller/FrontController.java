9932adaddd0d70549ac789762035f40e564c8b3b
L209_PC09\User <qlrhsid@naver.com>
2021-02-19 16:57:45 +0900

    여기다가 작업하시면 됩니다.

---
+++ BookInfo/src/com/java/controller/FrontController.java
@@ -0,0 +1,110 @@
+package com.java.controller;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Properties;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.java.command.Command;
+
+@WebServlet(urlPatterns="*.do", initParams=@WebInitParam(name="configFile", value="/WEB-INF/URICommand.properties"))
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private HashMap<String, Object> commandMap = new HashMap<String, Object>();
+
+    public FrontController() { super(); }
+    
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		
+		String configFile = config.getInitParameter("configFile");
+		Command.logger.info(Command.logMsg + configFile);
+		
+		FileInputStream fis = null;
+		BufferedInputStream bis = null;
+		Properties pro = new Properties();
+		
+		try {
+			
+			String path = config.getServletContext().getRealPath(configFile);
+			fis = new FileInputStream(path);
+    		bis = new BufferedInputStream(fis);
+    		pro.load(bis);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try{
+				
+	    		if(bis != null) bis.close();
+	    		if(fis != null) fis.close();
+    		}catch(Exception e){
+    			e.printStackTrace();
+    		}
+		}
+		
+		Iterator<Object> keyIter = pro.keySet().iterator();
+    	while(keyIter.hasNext()){
+    		String key = (String)keyIter.next();
+    		String value = pro.getProperty(key);
+    		
+    		Command.logger.info(Command.logMsg + key);
+    		Command.logger.info(Command.logMsg + value + "\n");
+    		
+    		try {
+    			Class<?> hClass = Class.forName(value);
+    			Object hObj = hClass.newInstance();
+    			
+    			commandMap.put(key, hObj);
+    		}catch(Exception e) {
+    			e.printStackTrace();
+    		}
+    	}
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String cmd = request.getServletPath();
+		Command.logger.info(Command.logMsg +cmd);
+		
+		String viewPage = null;
+		
+		try {
+			Command com = (Command) commandMap.get(cmd);
+			viewPage = com.actionDo(request, response);
+			
+			Command.logger.info(Command.logMsg + viewPage);
+		}catch(Throwable e) {
+			e.printStackTrace();
+		}
+		
+		if(viewPage != null) {
+			RequestDispatcher rd = request.getRequestDispatcher(viewPage);
+			rd.forward(request, response);
+		}
+		
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
+
+
+
+
+
+
